dc26a1c0b6b1d1a2fa0422b7a8b40ce33884f7e7
vuong200514 <106741622+vuong200514@users.noreply.github.com>
2024-12-26 07:05:24 +0000

    as

---
+++ studentApp/app/Http/Controllers/Backend/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+
+    }
+
+    public function login(){
+        
+        return view('Backend.auth.login');
+    }
+
+
+}
--- studentApp/routes/web.php
+++ studentApp/routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AuthController;
 
 Route::get('/', function () {
     return view('home');
 });
-use App\Http\Controllers\StudentController;
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/test', [StudentController::class, 'listStudent']);
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('login', [AuthController::class, 'login']) -> name('auth.login');
+
+// Route::get('/test', [StudentController::class, 'listStudent']);
 // Route::get('/student', 'App\Http\Controllers\StudentController@listStudent');
-Route::post('/edit', 'App\Http\Controllers\EditController@index')->name('edit');
-Route::post('/editDeleteStudent/{id}', 'App\Http\Controllers\EditController@optStudent')->name('delete');
+// Route::post('/edit', 'App\Http\Controllers\EditController@index')->name('edit');
+// Route::post('/editDeleteStudent/{id}', 'App\Http\Controllers\EditController@optStudent')->name('delete');
